36d6980d0fe564405bad7c37cc15cd62329b3813
Dylan <hidylanj@gmail.com>
2018-04-05 18:22:49 +1000

    Creates course list

--- SEF/src/CreateCourse.java
+++ SEF/src/CreateCourse.java
@@ -9,19 +9,27 @@ public class CreateCourse {
         System.out.print("Enter how many courses do you wish to add: ");
         int numOfCourses = Integer.parseInt(scan.nextLine());
 
-        //puts the courses into an array
+        //puts the courses and rates into arrays
         String arrayOfCourses[] = new String[numOfCourses];
+        int ratesOfCourses[] = new int[numOfCourses];
         for (int i = 0; i < arrayOfCourses.length; i++) {
-            System.out.print("Enter the ID of course " + (i + 1) + " : ");
+            System.out.println("Enter the ID of course " + (i + 1) + " : ");
             arrayOfCourses[i] = scan.nextLine();
         }
 
+        for (int i = 0; i < arrayOfCourses.length; i++) {
+            System.out.println("Enter the rates of course " + (i + 1) + " : ");
+            ratesOfCourses[i] = scan.nextInt();
+        }
+
         try {
-            PrintWriter pr = new PrintWriter("file.txt");
+            PrintWriter pr = new PrintWriter("Courselist.txt");
 
             //Prints list to file.txt
             for (int i = 0; i < arrayOfCourses.length; i++) {
-                pr.println(arrayOfCourses[i]);
+                pr.print(arrayOfCourses[i]);
+                pr.print(", ");
+                pr.println(ratesOfCourses[i]);
             }
             pr.close();
         } catch (Exception e) {
